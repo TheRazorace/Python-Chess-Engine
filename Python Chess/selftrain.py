c4c63dc6620d3124e790c3ca1a20505d01fdaefa
TheRazorace <49875599+TheRazorace@users.noreply.github.com>
2021-04-30 05:04:25 +0300

    Add files via upload

--- Python Chess/selftrain.py
+++ Python Chess/selftrain.py
@@ -4,10 +4,9 @@ from fen_transformation import fen_transform
 from keras import models
 import random
 
-def selfplay(games_simed, model):
+def selfplay(games_simed, model, distributions, selfgames):
     
     sys_random = random.SystemRandom()
-    
     training_set = []
     training_labels = []
     
@@ -17,7 +16,7 @@ def selfplay(games_simed, model):
     
     for match in range(games_simed):
         
-        print("Match:", match)
+        print("Match:", match+1)
         game = Board()
         prediction_set = []
         moves_set = []
@@ -40,13 +39,9 @@ def selfplay(games_simed, model):
             else:
                 best_indices = (-predictions).argsort()[-(int(len(predictions)/4) + 1):][::-1]
                 
-            weights = [100]
-            len_best = len(best_indices)
-            prob = 100
-            for i in range(1, len_best):
-                prob = prob - int(prob/2.5)
-                weights.append(prob)
-                
+ 
+            weights = distributions[len(best_indices) - 1]
+ 
             stohastic_choice = sys_random.choices(best_indices, weights=weights)[0]
             
             moves_set.append(prediction_set[stohastic_choice])
@@ -58,47 +53,71 @@ def selfplay(games_simed, model):
         if game.board.is_checkmate():
             result = not game.board.turn
         
-        
-        if result==False:
+        if result=="draw":
+            draws += 1
+            
+        elif result==False:
             losses += 1
             for i in range(moves):
                 training_labels.append(-1.0) 
                 training_set.append(moves_set[i])
                 
-        elif result==True:
+        else:
             wins += 1
             for i in range(moves):
                 training_labels.append(1.0)
                 training_set.append(moves_set[i])
-                
-        else: 
-            draws += 1
+                           
                 
              
     print("Wins:", wins, "Draws:", draws, "Losses:", losses, "\n")
                 
-
     training_set = np.asarray(training_set)
     training_labels = np.asarray(training_labels)
+    new_selfgames = selfgames + losses + wins
     
-    return training_set, training_labels
+    return training_set, training_labels, new_selfgames
 
+def create_distributions():
+    
+    distributions = []
+    for length in range(1,30):
+        weights = [100]
+        prob = 100
+        for i in range(1, length):
+            prob = prob - int(prob/2.5)
+            weights.append(prob)
+        distributions.append(weights)
+     
+    return distributions
+        
 
 
 if __name__ == "__main__":
     
+    distributions = create_distributions()    
     games_simed = 100
     for i in range(100):
         print("\nBatch:", i+1, "\n")
         
+        file = open("selfgames.txt","r+")
+        selfgames = int(file.read())
+        file.close()
+        
         model = models.load_model("selftrain_model") 
-        training_set, training_labels = selfplay(games_simed, model)
+        training_set, training_labels, new_selfgames = selfplay(games_simed, model,
+        distributions, selfgames)
         
         if (len(training_set) > 0):
             model.fit(training_set, training_labels,
-              batch_size=40, epochs=10, verbose=2)
-        
+              batch_size=5, epochs=10, verbose=2)
+            
         model.save("selftrain_model")
+        
+        file = open("selfgames.txt","w+")
+        file.write(str(new_selfgames))
+        file.close()
+
         
     
     
